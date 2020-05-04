e8c76bfed348d63e312a3ef3ff776b11f63cc6d2
kalmegati <kalmegati@gmail.com>
2020-05-04 15:22:09 -0400

    added queries for scraping

--- path-2-frontend/src/phrases.js
+++ path-2-frontend/src/phrases.js
@@ -1,9 +1,49 @@
+// returns myArr as an array of html documents
 
-// returns a ances as a dict of ancestry names pointing to links
-let ancestries = Array.from(document.getElementsByTagName("h2")).filter(heading => heading.className === "title").map(race => race.getElementsByTagName("a")[race.getElementsByTagName("a").length-1])
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+let targetUrl = "https://2e.aonprd.com/Classes.aspx"
 
-let ances = {}
+let myArr = [];
+fetch(proxyUrl+targetUrl).then(blob => blob.text()).then(function (html) {
+	const parser = new DOMParser();
+	let doc = parser.parseFromString(html, 'text/html');
+    myArr.push(doc);
+});
 
-for (const ance of ancestries) {
-    ances[ance.innerText] = ance.getAttribute("href");
-}
+// --------------------------------------------------
+
+// returns ancestries as a dict of ancestry names pointing to links
+
+let ances = Array.from(document.getElementsByTagName("h2")).filter(heading => heading.className === "title").map(race => race.getElementsByTagName("a")[race.getElementsByTagName("a").length-1]);
+
+let ancestries = {};
+
+for (const ance of ances) {
+    ancestries[ance.innerText] = ance.getAttribute("href");
+};
+
+// --------------------------------------------------
+
+// returns backgrounds as a dict of background names pointing to links
+
+let backs = Array.from(document.getElementsByTagName("h1")).filter(heading => heading.className === "title").map(x => x.getElementsByTagName("a")[1]).filter(x => !!x);
+
+let backgrounds = {};
+
+for (const back of backs) {
+    backgrounds[back.innerText] = back.getAttribute("href");
+};
+
+// --------------------------------------------------
+
+// returns classes as a dict of class names pointing to links
+
+let clases = Array.from(document.getElementsByTagName("h1")[1].getElementsByTagName("a")).filter(link => link.href.includes("?ID"));
+
+let classes = {};
+
+for (const clas of clases) {
+    classes[clas.innerText] = clas.getAttribute("href");
+};
+
+// --------------------------------------------------
